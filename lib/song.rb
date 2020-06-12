2d749b806e5c6c2f967cc72ec14a703d71fc5ba1
Kevin C Malin <racmalin@yahoo.com>
2020-06-12 15:51:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,9 +34,13 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-     
+      #if self.find_by_name(name) == nil 
+        #self.create_by_name(name)
+      #else
+        #self.find_by_name(name)
+      #end
       self.find_by_name(name) || self.create_by_name(name)
-      st thing is not true and the second thing is true)
+      #the above statement is saying do this(if it is true) or that (if the first thing is not true and the second thing is true)
   end
   
   def self.alphabetical()
